e0fa386d60a2b9b0d9cf9ea67bf7804755f3e241
Kirill Artamonov <artamonov.kirill@gmail.com>
2020-09-25 14:09:33 +0200

    :white_check_mark: re-arrange tests based on expected outcomes

--- src/test/groovy/S3Spec.groovy
+++ src/test/groovy/S3Spec.groovy
@@ -25,8 +25,8 @@ class S3Spec extends Specification {
         awsProviderVersion | localstackVersion
         '2.70.0'           | '0.11.3'
         '2.70.0'           | 'latest'
-        '3.1.0'            | 'latest'
-        '3.4.0'            | 'latest'
+        '3.1.0'            | '0.11.5'
+        '3.8.0'            | '0.11.5'
     }
 
     @Unroll
@@ -49,10 +49,12 @@ class S3Spec extends Specification {
 
         where:
         awsProviderVersion | localstackVersion
+        '3.0.0'            | '0.11.3' // https://github.com/localstack/localstack/issues/2832
+        '3.8.0'            | '0.11.3' // https://github.com/localstack/localstack/issues/2832
+        '3.1.0'            | '0.11.4' // https://github.com/localstack/localstack/issues/2832
+        '3.8.0'            | '0.11.4' // https://github.com/localstack/localstack/issues/2832
         '3.0.0'            | 'latest'
-        '3.0.0'            | '0.11.3'
-        '3.1.0'            | '0.11.3'
-        '3.4.0'            | '0.11.3'
+        '3.8.0'            | 'latest'
     }
 
 }
