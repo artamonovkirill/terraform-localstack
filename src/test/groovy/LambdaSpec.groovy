613efdee8dde307200a603f4f3c011999fec8428
Kirill Artamonov <artamonov.kirill@gmail.com>
2020-10-12 13:48:57 +0200

    :green_heart: join lambda tests after was fixed - https://github.com/localstack/localstack/issues/3037

--- src/test/groovy/LambdaSpec.groovy
+++ src/test/groovy/LambdaSpec.groovy
@@ -37,43 +37,7 @@ class LambdaSpec extends Specification {
         localstack.stop()
 
         where:
-        version << ['0.11.5']
-    }
-
-    @Unroll
-    def 'fails to invoke a lambda with #version LocalStack'(version) {
-        given:
-        def localstack = new LocalStack(version)
-        localstack.start()
-        and:
-        def s3 = AWS.s3(localstack)
-        s3.createBucket { it.bucket(stateBucket) }
-        and:
-        Terraform.Provider.generate(localstack, stateBucket)
-        Terraform.Module.generate('lambda')
-        Terraform.init()
-        and:
-        def lambda = AWS.lambda(localstack)
-
-        when:
-        def apply = Terraform.apply()
-
-        then:
-        apply.exitValue == 0
-
-        when:
-        def result = lambda.invoke { it.functionName('test') }
-
-        then:
-        result.statusCode() == 200
-        result.functionError()
-
-        cleanup:
-        print result.payload().asUtf8String()
-        localstack.stop()
-
-        where:
-        version << ['latest']
+        version << ['0.11.5', 'latest']
     }
 
 }
